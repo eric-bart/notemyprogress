cd50ce3c5d03b0c15c0eca87eb9c31283a2674e1
Éric <84646028+eric-bart@users.noreply.github.com>
2021-05-23 00:55:55 +0200

    Update logs.php

--- classes/logs.php
+++ classes/logs.php
@@ -1,6 +1,27 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-@copyright   2021 Éric Bart <eric.bart@etu.univ-tlse3.fr>, 2020 Edisson Sigua <edissonf.sigua@gmail.com>, Bryan Aguilar <bryan.aguilar6174@gmail.com>
+/**
+ * FlipLearning Logs component
+ *
+ * @package     local_fliplearning
+ * @autor       Edisson Sigua, Bryan Aguilar
+ * @copyright   2021 Éric Bart <eric.bart@etu.univ-tlse3.fr>, 2020 Edisson Sigua <edissonf.sigua@gmail.com>, Bryan Aguilar <bryan.aguilar6174@gmail.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 namespace local_fliplearning;
 
